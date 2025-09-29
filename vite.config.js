5e9eb3e6d5c48bf9b29543979a9151ffb467e72b
Pierre BEHEYT <pierre.beheyt.bba@edhec.com>
2025-09-29 16:41:26 +0200

    update host to allow cross device

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 8080,
-    proxy: {
-      '/socket.io': {
-        target: 'http://127.0.0.1:3004',
-        ws: true,
+export default defineConfig(({ mode }) => {
+  // Charge le fichier .env dans le répertoire de travail actuel
+  const env = loadEnv(mode, process.cwd(), '');
+  const serverUrl = env.VITE_SERVER_URL || 'http://127.0.0.1:3004';
+
+  console.log(`[Vite Config] Le proxy pour socket.io est configuré sur : ${serverUrl}`);
+
+  return {
+    plugins: [vue()],
+    server: {
+      port: 8080,
+      proxy: {
+        '/socket.io': {
+          target: serverUrl,
+          ws: true,
+        },
       },
     },
-  },
+  }
 })
